9fe2376823ba1e71af4a92e711a38015a75e0f6b
Minh Ngọc <tranductam2802@gmail.com>
2015-05-25 15:27:06 +0700

    Initial commit

---
+++ app/src/main/java/lbs/ntq/connector/MainActivity.java
@@ -0,0 +1,14 @@
+package lbs.ntq.connector;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_main);
+    }
+}
